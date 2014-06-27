e92710329e94b55b55c3afebace413fb12587ef2
AlloVince <allo.vince@gmail.com>
2014-06-27 15:38:38 +0800

    add livenews

--- apps/WscnWap/src/WscnWap/Controllers/IndexController.php
+++ apps/WscnWap/src/WscnWap/Controllers/IndexController.php
@@ -34,6 +34,20 @@ class IndexController extends ControllerBase
         $this->view->setVar('page', $page);
     }
 
+    public function livenewsAction()
+    {
+        $page = $this->request->getQuery('page', 'int', '0');
+        $provider  = Request::getProvider();
+        $provider->setBaseUri('http://api.wallstreetcn.com/apiv1/');
+        $response = $provider->get('livenews-list-v2.json', array(
+            'page' => $page,
+        ));
+        //$response->header->statusCode;
+        $posts = json_decode($response->body);
+        $this->view->setVar('posts', $posts);
+        $this->view->setVar('page', $page);
+    }
+
     public function nodeAction()
     {
         $id = $this->dispatcher->getParam('id');
