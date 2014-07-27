57cd2f8b33e5201a0f6d91618416bb9f8b8781d8
AlloVince <allo.vince@gmail.com>
2014-07-27 15:07:29 +0800

    fix test name

--- modules/EvaUser/tests/EvaUserTest/EvaUserTest.php
+++ modules/EvaUser/tests/EvaUserTest/EvaUserTest.php
@@ -5,7 +5,7 @@ use Eva\EvaEngine\Engine;
 use Eva\EvaEngine\Exception;
 use Phalcon\Config;
 
-class EvaCommonTest extends \PHPUnit_Framework_TestCase
+class EvaUserTest extends \PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
