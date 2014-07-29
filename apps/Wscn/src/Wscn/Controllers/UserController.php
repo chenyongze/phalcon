43e8a166670773a194f7dcdc6d943b2932f46fce
sunshine <sunsheng_it@163.com>
2014-07-29 16:00:21 +0800

    添加用户中心页面

---
+++ apps/Wscn/src/Wscn/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Wscn\Controllers;
+
+use Eva\EvaBlog\Models\Post;
+use Eva\EvaEngine\Exception;
+
+class UserController extends ControllerBase
+{
+    public function indexAction()
+    {
+    }
+
+    public function loginAction()
+    {
+    }
+
+}
