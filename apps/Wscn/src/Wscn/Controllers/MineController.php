e54ede41f62c7260a7c788780239294959e8f04f
AlloVince <allo.vince@gmail.com>
2014-08-11 15:02:57 +0800

    add profile

--- apps/Wscn/src/Wscn/Controllers/MineController.php
+++ apps/Wscn/src/Wscn/Controllers/MineController.php
@@ -3,6 +3,7 @@
 namespace Wscn\Controllers;
 
 use Eva\EvaUser\Models\Login;
+use Eva\EvaUser\Models\User;
 use Eva\EvaEngine\Mvc\Controller\SessionAuthorityControllerInterface;
 
 class MineController extends ControllerBase implements SessionAuthorityControllerInterface
@@ -10,6 +11,22 @@ class MineController extends ControllerBase implements SessionAuthorityControlle
     public function dashboardAction()
     {
         $user = Login::getCurrentUser();
+    }
+
+    public function profileAction()
+    {
+        $me = Login::getCurrentUser();
+        $user = User::findFirstById($me['id']);
+        $form = new \Eva\EvaUser\Forms\UserForm();
+        $form->setModel($user);
+        $form->addForm('profile', 'Eva\EvaUser\Forms\ProfileForm');
+        $this->view->setVar('item', $user);
+        $this->view->setVar('form', $form);
+    }
+
+    public function passwordAction()
+    {
+    
     }
 
 }
