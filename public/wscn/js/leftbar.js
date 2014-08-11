e9f8518f944f61f6b6736e002506c7d41a1d23ae
sunshine <sunsheng_it@163.com>
2014-08-11 09:49:07 +0800

    修改文章页 分享js代码

--- public/wscn/js/leftbar.js
+++ public/wscn/js/leftbar.js
@@ -1,3 +1,37 @@
+(function(root, factory) {
+    if (typeof define === "function" && define.amd) {
+        //For requirejs
+        define('widget', function (require, exports, module) {
+            var $ = require('jquery');
+            return factory(root, exports, $);
+        });
+    } else if (typeof exports !== "undefined") {
+        //For nodejs
+        var $ = require("jquery");
+        factory(root, exports, $);
+    } else {
+        //exports as global
+        root.widget = factory(root, {}, root.jQuery);
+    }
+}(this, function(root, widget, $) {
+    "use strict";
+    widget = function(){
+    };
+
+    widget.foo = function(){
+        console.log('widget compatible version foo', $.now());
+    };
+
+    widget.prototype = {
+        bar: function(){
+            console.log('widget compatible version bar', $.now());
+        }
+    };
+
+    return widget;
+}));
+
+
 /**
  * Created by Sun on 14-7-22.
  */
