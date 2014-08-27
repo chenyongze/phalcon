a23f0047f806d253a175359ace3caac495bab1d5
mr5 <u@mr-5.cn>
2014-08-27 16:53:04 +0800

    counter persistence

---
+++ utilities/persist_blog_posts_counter.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# 博客文章计数器持久化任务
+###### S: 定位工作路径 ######
+work_root=${0}
+# aboslute path
+if [[ ${work_root} == /* ]]
+then
+    work_root=`dirname ${work_root}`/../
+else
+    work_root=`dirname $(pwd)/${0}`/../
+fi
+cd ${work_root}
+###### E: 定位工作路径 ######
+./console.php wscn  EvaBlog Counter:persist
