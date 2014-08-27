9b9161072be9809856d00332b17c0224e0943d35
isunsheng <sunsheng@wallstreetcn.com>
2014-08-27 09:53:21 +0800

    修改 grunt 配置

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
                     strictMath: true,
                     sourceMap: true,
                     outputSourceFiles: true,
-                    sourceMapURL: 'style.css.map',
+                    sourceMapURL: 'public/static/wscn/css/style.css.map',
                     sourceMapFilename: 'style.css.map'
                 },
                 files: {
